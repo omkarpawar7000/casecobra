3adf40d6a9be6d8f922ce204ff00141e74508298
Omkar <omkarpawar7000@gmail.com>
2024-08-13 00:14:08 +0530

    initial commit and updated favicon ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,11 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Providers";
+import { constructMetadata } from "@/lib/utils";
 
 const recursive = Recursive({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "CaseCobra",
-  description: "A Modern E-Commerce store for Custom Phone Cases",
-};
+export const metadata: Metadata = constructMetadata()
 
 export default function RootLayout({
   children,
