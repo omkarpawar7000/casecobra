78e602dac3e8476e475698af149100de4a62f30c
Omkar <omkarpawar7000@gmail.com>
2024-08-12 22:41:48 +0530

    initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = async () => {
                                     Sign out
                                 </Link>
                                 {isAdmin ? (
-                                    <Link href='/api/auth/logout' className={buttonVariants({
+                                    <Link href='/dashboard' className={buttonVariants({
                                         size: 'sm', variant: 'ghost'
                                     })}>
                                         Dashboard ✨
